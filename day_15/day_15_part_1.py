ac12e70b892f34050aba042b27be5b179e0d5e5b
jkerney <justinkerney@192-168-1-188.tpgi.com.au>
2022-12-21 21:39:31 +1100

    setup day 15

---
+++ day_15/day_15_part_1.py
@@ -0,0 +1,23 @@
+"""
+Advent of Code 2022 - Day 15 Part 1
+https://adventofcode.com/2022/day/15
+"""
+
+#from ast import literal_eval
+#import numpy
+import itertools
+
+FILE_PATH = "day_15/input.txt"
+#FILE_PATH = "day_15/test.txt"
+
+def main():
+    '''
+    Day 15 Part 1
+    '''
+    with open(FILE_PATH, "r", encoding="utf-8") as input_file:
+
+        text_data = input_file.read()
+        text_data = text_data.splitlines()
+
+if __name__ == "__main__":
+    main()
