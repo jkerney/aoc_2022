c7aef5ab2e2462f1a39f9ebc5febbb9d703ef735
Justin Kerney <jkerney@gmail.com>
2022-12-28 23:34:50 +1100

    Day 9 2 (#17)
    * day 9 part 1 done
    
    * day 9 part 2 complete

--- day_9/day_9.py
+++ day_9/day_9.py
@@ -1,6 +1,9 @@
 '''
 Advent of Code - Day 9 - Part 1
 
+## Part 1
+
+
 # Input
 A sequence of moves. Example format 'R 4'.
 The first character is one of the four directions: U, D, L, R.
@@ -20,13 +23,27 @@ If the head and tail aren't touching:
     - If head is in a different row and column, then the tail moves one
     position diagonally towards the head
 
-# Output - Part 1
+# Output
+How many positions does the tail visit at least once?
+
+## Part 2
+
+# Calculation
+The head and tail are now connected by a chain of nodes.
+There are 8 nodes between the head and tail
+The movement of each node and its parent node/head is the same as part 1
+
+# Data structures
+Store the positions of the nodes and tail in a nested list
+
+# Output
 How many positions does the tail visit at least once?
 
 '''
 
 FILE_PATH = "day_9/input.txt"
 #FILE_PATH = "day_9/test.txt"
+#FILE_PATH = "day_9/test_2.txt"
 
 def update_tail(head_pos, tail_pos):
     '''
@@ -83,6 +100,7 @@ def main():
         moves = [move.split(' ') for move in moves]
         moves = [(move[0], int(move[1])) for move in moves]
 
+        # Part 1
         # Iterate through moves and calculate position of the head and tail
         # Head and tail start at (0,0)
         head_pos = [0,0]
@@ -104,7 +122,31 @@ def main():
 
         # Count how many positions the tail visits at least once
         tail_pos_record = [(x,y) for x,y in tail_pos_record]
-        print(len(set(tail_pos_record)))
+        print("Part 1:", len(set(tail_pos_record)))
+
+        # Part 2
+        # Iterate through moves and calculate position of the head, nodes,
+        # and tail
+        node_pos = [[0,0] for _ in range(10)]
+        tail_pos_record = []
+        for direction, itr in moves:
+            for _ in range(itr):
+                if direction == 'U':
+                    node_pos[0][1] += 1
+                elif direction == 'D':
+                    node_pos[0][1] -= 1
+                elif direction == 'L':
+                    node_pos[0][0] -= 1
+                elif direction == 'R':
+                    node_pos[0][0] += 1
+
+                for i in range(1, len(node_pos)):
+                    node_pos[i] = update_tail(node_pos[i-1], node_pos[i])
+
+                tail_pos_record.append((node_pos[i][0], node_pos[i][1]))
+
+        # Count how many positions the tail visits at least once
+        print("Part 2:", len(set(tail_pos_record)))
 
 if __name__ == "__main__":
     main()
